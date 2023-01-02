79966422bf61202881226d04b70b7de54a271d37
Thales Fossalussa <thalesvitor2001@gmail.com>
2023-01-02 17:08:49 -0300

    Initial commit

---
+++ app/dist/js/app.js
@@ -0,0 +1,22 @@
+import { NegociacaoController } from './controllers/negociacao-controller.js';
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+if (form) {
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        controller.adiciona();
+    });
+}
+else {
+    throw Error('Não foi possível inicializar a aplicação. Verifique se o form existe.');
+}
+const btnImporta = document.querySelector('#btn-importa');
+if (btnImporta) {
+    btnImporta.addEventListener('click', event => {
+        controller.importaDados();
+    });
+}
+else {
+    throw Error('Botão importa não encontrado.');
+}
+//# sourceMappingURL=app.js.map
---
+++ app/dist/js/utils/imprimir.js
@@ -0,0 +1,6 @@
+export function imprimir(...objetos) {
+    for (let objeto of objetos) {
+        console.log(objeto.toString());
+    }
+}
+//# sourceMappingURL=imprimir.js.map
